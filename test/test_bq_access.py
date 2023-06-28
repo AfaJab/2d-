76f56dc12ea025e205403f7386dbb164975e1d5a
Aloys Bernard <aloys.bernard@gmail.com>
2023-06-28 17:46:01 +0200

    Adding test to gcp

---
+++ test/test_bq_access.py
@@ -0,0 +1,40 @@
+import os
+from google.cloud import bigquery
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+PROJECT_ID = os.environ.get("PROJECT_ID")
+DATASET_ID = os.environ.get("DATASET_ID")
+
+
+full_dataset_id = f"{PROJECT_ID}.{DATASET_ID}"
+
+dataset = client.get_dataset(full_dataset_id)  # Make an API request.
+
+full_dataset_id = "{}.{}".format(dataset.project, dataset.dataset_id)
+friendly_name = dataset.friendly_name
+print(
+    "Got dataset '{}' with friendly_name '{}'.".format(
+        full_dataset_id, friendly_name
+    )
+)
+
+# View dataset properties.
+print("Description: {}".format(dataset.description))
+print("Labels:")
+labels = dataset.labels
+if labels:
+    for label, value in labels.items():
+        print("\t{}: {}".format(label, value))
+else:
+    print("\tDataset has no labels defined.")
+
+# View tables in dataset.
+print("Tables:")
+tables = list(client.list_tables(dataset))  # Make an API request(s).
+if tables:
+    for table in tables:
+        print("\t{}".format(table.table_id))
+else:
+    print("\tThis dataset does not contain any tables.")
